6dd0284a2141bd814994827f57d311b1115e97ba
Flutura Birinxhiku <fluturabirinxhiku@gmail.com>
2023-06-16 15:11:46 +0200

    Initial commit

---
+++ src/main/java/com/bookmarkmanagerbackend/BookmarkManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bookmarkmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmarkManagerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookmarkmanagerbackend/BookmarkManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmarkmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
